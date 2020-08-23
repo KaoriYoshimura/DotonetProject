96f4984cadb20d6c6d914ce645a5a0c95d538aca
KaoriYoshimura <infmalbyevo@gmail.com>
2020-08-23 08:00:49 +0200

    set up security

--- DotnetProject.API/Controllers/ValuesController.cs
+++ DotnetProject.API/Controllers/ValuesController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DotnetProject.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotnetProject.API.Controllers
 {
-    // http:localhost:5000/api/values
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -28,6 +29,7 @@ namespace DotnetProject.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValues(int id)
         {
